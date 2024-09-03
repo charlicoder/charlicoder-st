c93ece7d992469fdae4ff93916252f9d88780297
K Md Mamunur Rashid <charlicoder@gmail.com>
2024-09-03 19:50:43 +0600

    added deploy.sh script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+environment=dev
+date=$(date '+%Y-%m-%d-%H-%M')
+
+### Pull branch
+echo "Please enter branch name to pull:"
+read branch
+echo you have entered branch : $branch
+sleep 1
+git stash; git branch -f origin/$branch; git checkout $branch; git pull;
+
+echo "creating docker image"
+docker build -t charlicoder-st -f Dockerfile .
+
+### Complete Deployment
+echo "killing the running docker"
+docker ps -a | egrep 'charlicoder-st' | awk '{print $1}'| xargs docker kill
+docker ps -a | egrep 'charlicoder-st' | awk '{print $1}'| xargs docker rm
+
+echo "running the charlicoder using docker"
+docker run -d --restart=unless-stopped --name charlicoder-st -p 8000:8000 charlicoder-st
+
+echo "We are done !"
